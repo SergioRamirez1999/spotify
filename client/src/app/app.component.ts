745dadf3d5cf35b43df77ff5182c4bfa84f019ea
SergioRamirez <ramirezsergiod1999@outlook.com>
2018-07-09 02:24:07 -0300

    se agrega vista de login y signup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+
 })
 export class AppComponent {
-  title = 'amercasassssdasd!';
+    public title = 'MUSIFY';
+    public user: User;
+    //identity representara a la sesion del usuario, estara almacenado en local storage
+    public identity;
+    public token;
+
+    constructor() {
+        this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+
+    }
+
+    public onSubmit(){
+        console.log(this.user)
+    }
 }
